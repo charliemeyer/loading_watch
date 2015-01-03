6dc3577ce9efb075106aca2fb73a60d4fefb973d
Charles Meyer <charles.meyer@tufts.edu>
2015-01-03 00:31:52 -0500

    first release

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 
 static Window *s_main_window;
 static BitmapLayer* s_timeline_layers[12];
+static BitmapLayer *s_timeborder_layer;
+static GBitmap *s_timeborder_bitmap;
 
 int main() {
         init();
@@ -19,11 +21,18 @@ static void main_window_load(Window *window) {
         struct tm *current_time = localtime(&temp);
         int hour = current_time->tm_hour % 12;
         window_set_fullscreen(window, true);
+        
+        s_timeborder_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
+        s_timeborder_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+        bitmap_layer_set_bitmap(s_timeborder_layer, s_timeborder_bitmap);
+        
+        
         for(int i = 0; i < hour; i++){
-                s_timeline_layers[i] = bitmap_layer_create(GRect(0, i*14 + 1, 144, 13));
-                bitmap_layer_set_background_color(s_timeline_layers[i], GColorBlack);
+                s_timeline_layers[i] = bitmap_layer_create(GRect(16, i*14 + 1, 144, 13));                
+                bitmap_layer_set_background_color(s_timeline_layers[i], GColorBlack);       
                 layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_timeline_layers[i]));
         }
+        layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_timeborder_layer));
 }
 
 static void main_window_unload(Window *window) {
@@ -51,8 +60,10 @@ static void update_time() {
         struct tm *current_time = localtime(&temp);
         int hour = current_time->tm_hour % 12;
         int minute =  current_time-> tm_min;
-        bitmap_layer_destroy(s_timeline_layers[hour]);
-        int width = minute * 144 / 60;
+        if(minute > 0){
+                bitmap_layer_destroy(s_timeline_layers[hour]);        
+        }
+        int width = minute * 128 / 60;
         s_timeline_layers[hour] = bitmap_layer_create(GRect(0, hour*14 + 1, width, 13));
         bitmap_layer_set_background_color(s_timeline_layers[hour], GColorBlack);
         layer_add_child(window_get_root_layer(s_main_window), bitmap_layer_get_layer(s_timeline_layers[hour]));
